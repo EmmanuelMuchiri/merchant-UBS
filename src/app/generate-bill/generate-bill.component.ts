65dcecefdc3a181ab71c19409e7480954ef3760f
Sara Munini <saramunini11@gmail.com>
2019-11-21 18:14:58 +0300

    generate bill works perfectly

--- src/app/generate-bill/generate-bill.component.ts
+++ src/app/generate-bill/generate-bill.component.ts
@@ -1,30 +1,30 @@
-// import { Component, Input, OnInit } from '@angular/core';
-// import { ApiService } from '../api.service';
-// import { Bill } from '../postdataObj';
-// import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Bill } from '../postdataObj';
+import { Router } from '@angular/router';
 
 
-// @Component({
-//   selector: 'app-generate-bill',
-//   templateUrl: './generate-bill.component.html',
-//   styleUrls: ['./generate-bill.component.css']
-// })
+@Component({
+  selector: 'app-generate-bill',
+  templateUrl: './generate-bill.component.html',
+  styleUrls: ['./generate-bill.component.css']
+})
 
 
-// export class GenerateBillComponent implements OnInit {
+export class GenerateBillComponent implements OnInit {
 
-//   @Input() newBill = { name: '', phone: 0, email: '', narration: '', generated_by: ''}
+  @Input() newBill = { name: '', phone: 0, email: '', narration: '', generated_by: ''}
 
-//     constructor(private apiservice:ApiService){ }
+    constructor(private apiservice:ApiService){ }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
 
-//   addBill(dataBill) {
-//     this.apiservice.generateBill(this.newBill).subscribe((data: {}) => {
-//     })
-//   }
+  addBill(dataBill) {
+    this.apiservice.generateBill(this.newBill).subscribe((data: {}) => {
+    })
+  }
   
-// }
+}
 
